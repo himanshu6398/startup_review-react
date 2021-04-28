63582df04ec1c5e0f91a40c05450f70360ce6541
himanshu <himanshusingh6398@gmail.com>
2021-04-28 18:40:26 +0530

    update delete fixed

--- src/services/startup.service.js
+++ src/services/startup.service.js
@@ -9,13 +9,26 @@ const addStartup = (startup) => {
 const getStartups = (params)=>{
     return axios.get(API_URL,{params});
 };
+const displayStartups = () => {
+    return axios.get(API_URL + "all");
+};
 const get = id => {
     return axios.get(API_URL+id);
 };
+
+const updateStartup = (id,startup) => {
+    return axios.put(API_URL + "update/"+id, startup, {headers: authHeader()});
+};
+
+const deleteStartup =(id) => {
+    return axios.delete(API_URL + "delete/"+id, {headers: authHeader()});
+}
 
 export default {
     addStartup,
     getStartups,
     get,
-
+    updateStartup,
+    displayStartups,
+    deleteStartup,
 };
